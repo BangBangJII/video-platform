c231fa37869b7b1f85a0fffc8bcb13555cf51c23
Qunce Hua <100831648+BangBangJII@users.noreply.github.com>
2025-06-24 19:53:57 -0700

    initial

---
+++ bilibili/bilibili-service/src/main/java/com/example/bilibili/service/FollowingGroupService.java
@@ -0,0 +1,30 @@
+package com.example.bilibili.service;
+
+import com.example.bilibili.dao.FollowingGroupDao;
+import com.example.bilibili.domain.FollowingGroup;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FollowingGroupService {
+    @Autowired
+    private FollowingGroupDao followingGroupDao;
+
+    public FollowingGroup getByType(String type){
+        return followingGroupDao.getByType(type);
+    }
+    public FollowingGroup getById(Long id){
+        return followingGroupDao.getById(id);
+    }
+    public List<FollowingGroup> getByUserId(Long userId){
+        return followingGroupDao.getByUserId(userId);
+    }
+    public void addFlowingGroup(FollowingGroup flowingGroup){
+        followingGroupDao.addFlowingGroup(flowingGroup);
+    }
+    public List<FollowingGroup> getUserFollowingGroups(Long userId){
+        return followingGroupDao.getUserFollowingGroups(userId);
+    }
+}
